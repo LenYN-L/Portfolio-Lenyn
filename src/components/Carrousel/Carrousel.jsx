0e463e2644d488ce0800b86ba72475a701abd358
Leon4RD <leon4rd@pop-os.localdomain>
2023-02-20 21:59:20 -0500

    first commit

---
+++ src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,66 @@
+import { useState, useRef, useEffect } from "react";
+import img1 from '../../../public/images-profile/img1.jpeg'
+import img2 from '../../../public/images-profile/img2.jpeg'
+import img3 from '../../../public/images-profile/img3.jpeg'
+
+import './Carrousel.css'
+
+const colors = [img1, img2];
+const delay = 2500;
+
+const Carrousel = () => {
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
+  
+    function resetTimeout() {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    }
+  
+    useEffect(() => {
+      resetTimeout();
+      timeoutRef.current = setTimeout(
+        () =>
+          setIndex((prevIndex) =>
+            prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+          ),
+        delay
+      );
+  
+      return () => {
+        resetTimeout();
+      };
+    }, [index]);
+  
+    return (
+      <div className="slideshow">
+        <div
+          className="slideshowSlider"
+          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+        >
+          {colors.map((backgroundColor, index) => (
+            <div
+              className="slide"
+              key={index}
+              style={{ backgroundColor }}
+            ><img className="mix-blend-multiply grayscale w-96" src={backgroundColor} alt="" /></div>
+          ))}
+        </div>
+  
+        <div className="slideshowDots">
+          {colors.map((_, idx) => (
+            <div
+              key={idx}
+              className={`slideshowDot${index === idx ? " active" : ""}`}
+              onClick={() => {
+                setIndex(idx);
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default Carrousel
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			'presStart2': ['"Press Start 2P"', 'cursive'],
+			'AbrilFatface':['"Abril Fatface"', 'Arial'],
+			'SpecialElite': ['"Special Elite"', 'Arial']
+		},
+		extend: {},
+	},
+	plugins: [],
+}

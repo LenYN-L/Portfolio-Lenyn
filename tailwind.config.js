39b6eb76c67a0da3ccb382f39301d94f21171329
lenyn <lenyn-12-31@outlook.com>
2023-08-07 22:56:05 -0500

    Tercera Portada Nuevo Disenio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			'presStart2': ['"Press Start 2P"', 'cursive'],
+			'AbrilFatface':['"Abril Fatface"', 'Arial'],
+			'SpecialElite': ['"Special Elite"', 'Arial']
+		},
+		extend: {
+			animation: {
+				wiggle: 'wiggle 10s linear infinite',
+			  },
+			keyframes: {
+				wiggle: {
+				  '0%': { transform: 'translateX(-30vh)',opacity: '0%' }, 
+				  '50%': { transform: 'translateX(0vh)',opacity: '100%'},
+				  '100%': { transform: 'translateX(30vh)',opacity: '0%' },
+				}
+			  }
+		},
+	},
+	plugins: [],
+}
